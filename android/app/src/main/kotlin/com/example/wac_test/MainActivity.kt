5b2468bdb545eef3a29cd8b03a8aa65458531b01
Roshan Justin <77222525+roshanjustin0010@users.noreply.github.com>
2024-02-06 19:24:26 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/wac_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wac_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/features/homescreen/modal/homeScreenData.dart
@@ -0,0 +1,50 @@
+class HomeScreenData {
+  String? type;
+  String? title;
+  List<Contents>? contents;
+  String? id;
+
+  HomeScreenData({this.type, this.title, this.contents, this.id});
+
+  HomeScreenData.fromJson(Map<String, dynamic> json) {
+    type = json['type'];
+    title = json['title'];
+    if (json['contents'] != null) {
+      contents = <Contents>[];
+      json['contents'].forEach((v) {
+        contents!.add(new Contents.fromJson(v));
+      });
+    }
+    id = json['id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['type'] = this.type;
+    data['title'] = this.title;
+    if (this.contents != null) {
+      data['contents'] = this.contents!.map((v) => v.toJson()).toList();
+    }
+    data['id'] = this.id;
+    return data;
+  }
+}
+
+class Contents {
+  String? title;
+  String? imageUrl;
+
+  Contents({this.title, this.imageUrl});
+
+  Contents.fromJson(Map<String, dynamic> json) {
+    title = json['title'];
+    imageUrl = json['image_url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['title'] = this.title;
+    data['image_url'] = this.imageUrl;
+    return data;
+  }
+}
